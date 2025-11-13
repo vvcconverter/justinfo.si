d0466695ef44fa3e31fa0d3101858d0bae6a8d98
vvcconverter <bunduqias@gmail.com>
2025-11-13 12:46:51 +0300

    Update sw.js

--- vvc-converter/sw.js
+++ vvc-converter/sw.js
@@ -1,5 +1,25 @@
 const CACHE_NAME = 'vvc-cache-v5';
 const FILES_TO_CACHE = [
+  '/',
+  '/cache/',
+  '/cache/lib/',
+  '/cache/AsyncHelpers.js',
+  '/cache/VVCPlayer.mjs',
+  '/cache/lib/bootstrap.min.js',
+  '/cache/lib/mp4box.all.min.js',
+  '/cache/lib/three.module.min.js',
+  '/cache/lib/uPlot.esm.min.js',
+  '/img/',
+  '/img/apple-touch-icon.png',
+  '/img/favicon.ico',
+  '/img/favicon.svg',
+  '/img/VVC-Converter Telegram.svg',
+  '/img/vvc-converter.png',
+  '/img/web-app-manifest-192x192.png',
+  '/img/web-app-manifest-512x512.png',
+  '/index.html',
+  '/project_structure.txt',
+  '/webplayervvc-cache/',
   '/webplayervvc-cache/AsyncHelpers.js',
   '/webplayervvc-cache/bitstreams.json',
   '/webplayervvc-cache/bootstrap.min.js',
@@ -8,15 +28,14 @@ const FILES_TO_CACHE = [
   '/webplayervvc-cache/mp4box.all.min.js',
   '/webplayervvc-cache/project_structure.txt',
   '/webplayervvc-cache/shell.mjs',
+  '/webplayervvc-cache/street.mp4',
   '/webplayervvc-cache/styles.css',
   '/webplayervvc-cache/three.module.min.js',
   '/webplayervvc-cache/uPlot.esm.min.js',
   '/webplayervvc-cache/VVCPlayer.mjs',
   '/webplayervvc-cache/vvdecapp.js',
-  '/webplayervvc-cache/wasm_test-server.py',
   '/webplayervvc-cache/vvdecapp.wasm',
-  '/webplayervvc-cache/street.mp4',
-  '/sw.js'
+  '/webplayervvc-cache/wasm_test-server.py'
 ];
 
 self.addEventListener('install', event => {
